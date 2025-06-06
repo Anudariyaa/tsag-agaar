63da717e5b5bb786043f9bd6a765caf68022ea87
Anudariyaa <a.anudariya2026@shildeg.edu.mn>
2025-06-06 13:48:20 +0800

    done

---
+++ script.js
@@ -0,0 +1,36 @@
+const apiKey = "dfe2aa556109ea704928dec09dfc1283"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+const cityname = document.querySelector(".cityname")
+const searchBtn = document.querySelector(".search button")
+const hotiinNer = document.querySelector(".city")
+const degree = document.querySelector(".temp")
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
+    const data = await response.json()
+    hotiinNer.innerHTML = data.name
+    degree.innerHTML = data.main.temp + "℃"
+    if (data.weather [0].main === "Rain") {
+        weatherIcon.src = "zurag/rain.png"
+    } else if (data.weather [0].main === "Snow") {
+        weatherIcon.src = "zurag/snow.png"
+    }
+    else if (data.weather [0].main === "Clear") {
+        weatherIcon.src = "zurag/clear.png"
+    }
+    else if (data.weather [0].main === "Drizzle") {
+        weatherIcon.src = "zurag/drizzle.png"
+    }
+    else if (data.weather [0].main === "Mist") {
+        weatherIcon.src = "zurag/mist.png"
+    }
+
+    console.log(data)
+
+}
+searchBtn.addEventListener("click", ()=> {
+    checkWeather(cityname.value)
+    console.log(cityname.value)
+})
+   
